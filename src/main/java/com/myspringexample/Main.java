171615f96ca245df134f7a10755ca8a04e539878
Suresh <suresh9843@gmail.com>
2018-12-09 00:08:10 +0530

    fff

---
+++ src/main/java/com/myspringexample/Main.java
@@ -0,0 +1,24 @@
+package com.myspringexample;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		// loading the definitions from the given XML file
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		HelloWorldService service = (HelloWorldService) context.getBean("helloWorldService");
+		String message = service.sayHello();
+		System.out.println(message);
+
+		// set a new name
+		service.setName("Spring");
+		message = service.sayHello();
+		System.out.println(message);
+	}
+}
